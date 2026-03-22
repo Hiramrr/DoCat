4e757a7334a1f2807383f9f65e9df70818c14945
hiramrr <sasuke_77.23@hotmail.com>
2026-03-21 20:28:16 -0600

    se cambio el theme

--- app/src/main/java/com/smart/docat/core/utils/TimeFormatter.kt
+++ app/src/main/java/com/smart/docat/core/utils/TimeFormatter.kt
@@ -1,1 +1,21 @@
 package com.smart.docat.core.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TimeFormatter @Inject constructor() {
+
+    fun formatSeconds(seconds: Int): String {
+        val minutes = seconds / 60
+        val secs = seconds % 60
+        return "%02d:%02d".format(minutes, secs)
+    }
+
+    fun formatDate(date: Date = Date()): String {
+        return SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
+    }
+}
